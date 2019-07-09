eb5f3c41c693d86376de2020baf88a07828f9ff8
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-07-09 17:49:48 -0300

    povos tradicionais, scripts

---
+++ povos_tradicionais/dados_auxiliares.sql
@@ -0,0 +1,317 @@
+INSERT INTO POVOS_TRADICIONAIS_FAIXA_ET_MORADORES VALUES
+(1, 'Acima de 80 anos'),
+(2, '75 a 79 anos'),
+(3, '70 a 74 anos'),
+(4, '65 a 69 anos'),
+(5, '60 a 64 anos'),
+(6, '55 a 59 anos'),
+(7, '50 a 54 anos'),
+(8, '45 a 49 anos'),
+(9, '40 a 44 anos'),
+(10, '35 a 39 anos'),
+(11, '30 a 34 anos'),
+(12, '25 a 29 anos'),
+(13, '20 a 24 anos'),
+(14, '15 a 19 anos'),
+(15, '10 a 14 anos'),
+(16, '5 a 9 anos'),
+(17, '0 a 4 anos');
+
+INSERT INTO POVOS_TRADICIONAIS_SEXO_MORADOR VALUES
+(1, 'Feminino'),
+(2, 'Masculino');
+
+INSERT INTO GRAU_ESCOL_MORADOR VALUES
+(0, 'Analfabeto'),
+(1, 'Ensino fundamental'),
+(2, 'Ensino fundamental incompleto'),
+(3, 'Ensino médio'),
+(4, 'Ensino médio incompleto'),
+(5, 'Ensino superior completo'),
+(6, 'Ensino superior incompleto'),
+(7, 'Pós graduação');
+
+INSERT INTO POVOS_TRADICIONAIS_SEXO_ENTREVISTADO VALUES
+(0, 'Feminino'),
+(1, 'Masculino');
+
+INSERT INTO POVOS_TRADICIONAIS_FAIXA_ETAR_ENTREVISTADO VALUES
+(1, 'Acima de 80 anos'),
+(2, '75 a 79 anos'),
+(3, '70 a 74 anos'),
+(4, '65 a 69 anos'),
+(5, '60 a 64 anos'),
+(6, '55 a 59 anos'),
+(7, '50 a 54 anos'),
+(8, '45 a 49 anos'),
+(9, '40 a 44 anos'),
+(10, '35 a 39 anos'),
+(11, '30 a 34 anos'),
+(12, '25 a 29 anos'),
+(13, '20 a 24 anos'),
+(14, '15 a 19 anos');
+
+INSERT INTO POVOS_TRADICIONAIS_ESCOLARIDADE_ENTREVISTADO VALUES
+(0, 'Ensino fundamental incompleto'),
+(1, 'Ensino médio incompleto'),
+(2, 'Superior incompleto'),
+(3, 'Ensino fundamental completo'),
+(4, 'Ensino médio completo'),
+(5, 'Superior completo');
+
+INSERT INTO POVOS_TRADICIONAIS_EST_CIV_ENTREVISTADO VALUES
+(0, 'Solteiro (a)'),
+(1, 'Casado(a)'),
+(2, 'Separado(a)'),
+(3, 'Divorciado(a)'),
+(4, 'Viúvo(a)'),
+(5, 'União Estável');
+
+INSERT INTO POVOS_TRADICIONAIS_ATIV_PROF_ENTREVISTADO VALUES
+(0, 'Profissional Autônomo (a)'),
+(1, 'Agricultor (a)'),
+(2, 'Empregado/servidor público (a)'),
+(3, 'Desempregado (a)'),
+(4, 'Empresário'),
+(5, 'Estudante'),
+(6, 'Beneficiário'),
+(7, 'Outro');
+
+INSERT INTO POVOS_TRADICIONAIS_RENDA_FAMILIAR_ENTREVISTADO VALUES
+(0, 'Até 1 salário mínimo'),
+(1, 'Até 2 Salários mínimos'),
+(2, 'Até 5 Salários mínimos'),
+(3, 'Mais de 5 Salários mínimos'),
+(4, 'Mais de 10 Salários Mínimos'),
+(5, 'Sem renda');
+
+INSERT INTO POVOS_TRADICIONAIS_TIPO_POVOS_ENTREVISTADO VALUES
+(1, 'Quilombolas'),
+(2, 'Indígenas'),
+(3, 'Ribeirinhos'),
+(4, 'Assentados'),
+(5, 'Bolivianos'),
+(6, 'Agricultor');
+
+
+INSERT INTO POVOS_TRADICIONAIS_TEMP_RES_COMUN_ENTREVISTADO VALUES
+(0, 'Até 1 ano'),
+(1, 'Entre 1 e 2 anos'),
+(2, 'Entre 3 e 5 anos'),
+(3, 'Entre 5 e 10 anos'),
+(4, 'Mais de 10 anos');
+
+INSERT INTO POVOS_TRADICIONAIS_MATERIAL_CASA_ENTREV VALUES
+(0, 'Alvenaria'),
+(1, 'Madeira'),
+(2, 'Taipa'),
+(3, 'Alvenaria e madeira'),
+(4, 'Alvenaria e Taipa'),
+(5, 'Outro');
+
+INSERT INTO POVOS_TRADICIONAIS_EXISTE_ENERG_ELET_ENTREVISTADO VALUES
+(1, 'SIM'),
+(0, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_PCD_FAMILIA VALUES
+(1, 'SIM'),
+(0, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_FREQ_AGENTES VALUES
+(0, 'Nunca'),
+(1, 'Frequentemente'),
+(2, 'As vezes'),
+(3, 'Poucas vezes');
+
+INSERT INTO POVOS_TRADICIONAIS_REALIZA_IRRIG_ENTREVISTADO VALUES
+(1, 'SIM'),
+(0, 'NÃO'),
+(2, 'Não se aplica');
+
+INSERT INTO POVOS_TRADICIONAIS_FORM_ADUB_ITIL_AGRO_DES VALUES
+(0, 'Químicaa'),
+(1, 'Orgânica'),
+(2, 'Não se aplica');
+
+INSERT INTO POVOS_TRADICIONAIS_PROB_AGUA_UTIL VALUES
+(1, 'SIM'),
+(2, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_FREQ_FALTA_AGUA VALUES
+(1, 'De 1 a 2 horas'),
+(2, 'De 2 a 5 horas'),
+(3, 'Mais de 5 horas'),
+(4, 'Mais de 1 dia'),
+(5, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_PROBLEMAS_FREQ VALUES
+(1, 'Diariamente'),
+(2, 'Semanalmente'),
+(3, 'Esporadicamente');
+
+INSERT INTO POVOS_TRADICIONAIS_AGUA_GOSTO VALUES
+(0, 'Sempre boa'),
+(1, 'Quase sempre boa'),
+(2, 'Nunca boa'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_AGUA_VISUAL VALUES
+(0, 'Sempre boa'),
+(1, 'Quase sempre boa'),
+(2, 'Nunca boa'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_AGUA_CHEIRO VALUES
+(0, 'Sempre boa'),
+(1, 'Quase sempre boa'),
+(2, 'Nunca boa'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_TIPO_TRAT_AGUA VALUES
+(0, 'Filtro de água'),
+(1, 'Filtro de barro'),
+(2, 'Cloro'),
+(3, 'Coada'),
+(4, 'Fervida'),
+(5, 'Água tratada comprada em galão'),
+(6, 'Não recebe tratamento'),
+(7, 'Outro');
+
+INSERT INTO POVOS_TRADICIONAIS_FREQ_LIMPEZA_CX VALUES
+(0, 'Mensal'),
+(1, 'Semestral'),
+(2, 'Anual'),
+(3, 'Bianual'),
+(4, 'Não realiza'),
+(5, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_POSSUI_SANITARIO VALUES
+(0, 'Dentro de Casa'),
+(1, 'Fora de casa/ sanitário'),
+(2, 'Fora de casa/ ossa negra'),
+(3, 'Não possui');
+
+INSERT INTO POVOS_TRADICIONAIS_DEST_ESGOTO VALUES
+(0, 'Rede coletora de esgoto'),
+(1, 'Fossa séptica'),
+(2, 'Fossa Negra'),
+(3, 'Vala/Sarjeta'),
+(4, 'Galeria de águas pluviais'),
+(5, 'Igarapé/rios'),
+(6, 'Igarapé a céu aberto'),
+(7, 'Mato'),
+(8, 'Quintal'),
+(9, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_FREQ_LIMP_FOS VALUES
+(0, 'Mensal'),
+(1, 'Semestral'),
+(2, 'Anual'),
+(3, 'Bianual'),
+(4, 'Não realiza'),
+(5, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_SEP_ESGT_PIA_TANQ VALUES
+(1, 'SIM'),
+(2, 'NÃO'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_VAZAMENTO_ESGOT VALUES
+(1, 'SIM'),
+(2, 'NÃO'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_MAU_CHEIRO VALUES
+(1, 'SIM'),
+(2, 'NÃO'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_FONTE_CHEIRO VALUES
+(1, 'Não sei'),
+(2, 'Esgoto a céu aberto'),
+(3, 'Bueiro'),
+(4, 'Rio/Igarapé'),
+(5, 'Terreno baldio'),
+(6, 'Mato'),
+(7, 'Outro');
+
+INSERT INTO POVOS_TRADICIONAIS_LANC_CLANDESTINO VALUES
+(1, 'SIM'),
+(2, 'NÃO'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_LANC_CLAND VALUES
+(1, 'SIM'),
+(2, 'NÃO');
+
+
+INSERT INTO POVOS_TRADICIONAIS_IGARAPE_PROX VALUES
+(0, 'Sim, com vegetação protegendo'),
+(1, 'Sim, sem vegetação protegendo'),
+(2, 'Não há rio ou igarapé próximo'),
+(3, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_POSSUI_COLETA VALUES
+(1, 'SIM'),
+(2, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_SERV_COLETA_LX VALUES
+(1, 'SIM'),
+(2, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_FREQ_COLETA VALUES
+(0, 'Diariamente'),
+(1, 'Dias alternados'),
+(2, '1 vez na semana'),
+(3, '2 vezes na semana'),
+(4, '3 vezes na semana'),
+(5, 'Mais de 4 vezes na semana'),
+(6, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_DEST_ELETRONICOS VALUES
+(0, 'São descartados juntos com o lixo comum'),
+(1, 'São selecionados e encaminhados para o centro de reciclagem'),
+(2, 'São selecionados encaminhados para algum catador'),
+(3, 'São enterrados'),
+(4, 'São queimados'),
+(5, 'São lançados no rio/Igarapé'),
+(6, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_EXISTE_COLT_SELETIVA VALUES
+(1, 'Sim'),
+(0, 'Não'),
+(2, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_IMPORT_RECICLAR VALUES
+(0, 'Muito importante'),
+(1, 'Importante'),
+(2, 'Sem importância'),
+(3, 'Indiferente'),
+(4, 'Não sei');
+
+INSERT INTO POVOS_TRADICIONAIS_FREQ_MIDIA VALUES
+(0, 'Frequentemente'),
+(1, 'As vezes'),
+(2, 'Nunca');
+
+INSERT INTO POVOS_TRADICIONAIS_PARTICP_ASSOC VALUES
+(1, 'SIM'),
+(0, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_FREQ_REUNIOES VALUES
+(0, 'Nunca'),
+(1, 'Frequentemente'),
+(2, 'As vezes'),
+(3, 'Poucas vezes');
+
+INSERT INTO POVOS_TRADICIONAIS_FESTAS_REUNIOES_ENTREV  VALUES
+(1, 'SIM'),
+(2, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_PARTICIPA_GRP_WHATS VALUES
+(1, 'SIM'),
+(0, 'NÃO');
+
+INSERT INTO POVOS_TRADICIONAIS_QUER_GRP_WHATS VALUES
+(0, 'SIM'),
+(1, 'NÃO');
