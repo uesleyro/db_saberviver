53ec4972cd8384518c542c1aa52fa9318f6d47a0
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-07-16 16:34:50 -0300

    catadores

---
+++ catadores/dados_auxiliares.sql
@@ -0,0 +1,125 @@
+INSERT INTO CATADORES_FAIXA_ETAR_ENTREVISTADO_CTDRS VALUES
+(1,'Acima de 80 anos'),
+(2,'75 a 79 anos'),
+(3, '70 a 74 anos'),
+(4, '65 a 69 anos'),
+(5, '60 a 64 anos'),
+(6, '55 a 59 anos'),
+(7, '50 a 54 anos'),
+(8, '45 a 49 anos'),
+(9, '40 a 44 anos'),
+(10, '35 a 39 anos'),
+(11, '30 a 34 anos'),
+(12, '25 a 29 anos'),
+(13, '20 a 24 anos'),
+(14, '15 a 19 anos');
+
+INSERT INTO CATADORES_SEXO_CATADOR VALUES
+(0, 'Feminino'),
+(1, 'Masculino'),
+(2, 'Prefiro não dizer');
+
+INSERT INTO CATADORES_EST_CIVIL_CATADOR VALUES
+(0, 'Casado(a)'),
+(1, 'Solteiro(a)'),
+(2, 'Viúvo(a)'),
+(3, 'Divorciado(a)'),
+(4, 'Separado(a)'),
+(5, 'Outros(a)');
+
+INSERT INTO CATADORES_ESCOLARIDADE_CATADOR VALUES
+(0, 'Ensino fundamental incompleto'),
+(1, 'Ensino médio incompleto'),
+(2, 'Superior incompleto'),
+(3, 'Ensino fundamental completo'),
+(4, 'Ensino médio completo'),
+(5, 'Superior completo');
+
+INSERT INTO CATADORES_QUALIF_PROF_CATADOR VALUES
+(1, 'Aposentado/ pensionista'),
+(2, 'Trabalhador rural'),
+(3, 'Assalariado c/ carteira'),
+(4, 'Autônomo s/ previdência social'),
+(5, 'Assalariado sem carteira'),
+(6, 'Autônomo c/ previdência social'),
+(7, 'Benefício-BPC'),
+(8, 'Outro');
+
+INSERT INTO CATADORES_DOMICILIO_CATADOR VALUES
+(0, 'Dentro da área do lixão'),
+(1, 'No entorno do lixão'),
+(2, 'Próximo ao lixão'),
+(3, 'Longe da área do lixão');
+
+INSERT INTO CATADORES_SIT_IMOVEL_CATADOR VALUES
+(0, 'Próprio'),
+(1, 'Alugado'),
+(2, 'Invadido'),
+(3, 'Financiado');
+
+INSERT INTO CATADORES_FAM_ENV_COLET_CATADOR VALUES
+(1, 'SIM'),
+(0, 'NÃO');
+
+INSERT INTO CATADORES_PCD_FAMILIA VALUES
+(1, 'SIM'),
+(0, 'NÃO');
+
+INSERT INTO CATADORES_RENDA_FAMILIAR_CATADOR VALUES
+(0, 'Até um salário mínimo'),
+(1, 'Até 2 Salários mínimos'),
+(2, 'Até 5 Salários mínimos'),
+(3, 'Mais de 5 Salários mínimos'),
+(4, 'Mais de 10 Salários mínimos');
+
+INSERT INTO CATADORES_TRAB_MAN_FAM_CATADOR VALUES
+(0, 'SIM'),
+(1, 'NÃO');
+
+INSERT INTO CATADORES_DURACAO_TRAB_CATADOR VALUES
+(0, '4 horas'),
+(1, '6 horas'),
+(2, '8 horas'),
+(3, 'Acima de 8 horas');
+
+INSERT INTO CATADORES_CONHEC_LIXAO_PREF_CATADOR VALUES
+(0, 'SIM'),
+(1, 'NÃO');
+
+INSERT INTO CATADORES_ORGANIZACAO_CATADOR VALUES
+(0, 'Associação'),
+(1, 'Cooperativa'),
+(2, 'Informalmente'),
+(4, 'Trabalham de forma individualizada');
+
+INSERT INTO CATADORES_LIXAO_RESID_CATADOR VALUES
+(0, 'SIM'),
+(1, 'NÃO'),
+(2, 'NÃO SEI');
+
+INSERT INTO CATADORES_PROX_LIXAO_RES_CATADOR VALUES
+(0, 'SIM'),
+(1, 'NÃO'),
+(2, 'NÃO SEI');
+
+INSERT INTO CATADORES_MAIORCATARESIDEPROX VALUES
+(0, 'SIM'),
+(1, 'NÃO'),
+(2, 'NÃO SEI');
+
+INSERT INTO CATADORES_CATARESIDBAIRROSDIST VALUES
+(0, 'SIM'),
+(1, 'NÃO'),
+(2, 'NÃO SEI');
+
+INSERT INTO CATADORES_SEPARACAOCOMPONENTE VALUES
+(0, 'Catadores'),
+(1, 'Outros');
+
+INSERT INTO CATADORES_MEIODESOBREVIVENCIA VALUES
+(0, 'Vivem apenas disso'),
+(1, 'Possuem outro tipo de trabalho');
+
+INSERT INTO CATADORES_WHATSAPP_PROJETO VALUES
+(1, 'SIM'),
+(2, 'NÃO');
